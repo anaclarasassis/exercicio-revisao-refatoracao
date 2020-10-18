aecef7b20948818d5f4c8558443489806c28b217
anaclarasassis <73037273+anaclarasassis@users.noreply.github.com>
2020-10-18 18:18:51 -0300

    Update Empregado.hpp
    1- Adição dos metódos set e get;
    2 - Retirada de números mágicos;
    3 - Adição do atributo nome para classe mãe;
    4 - Retirada do atributo quotaMensalVendas;

--- Empregado.hpp
+++ Empregado.hpp
@@ -5,24 +5,49 @@
 #include <string>
 
 class Empregado {
-	
-  public:
-    double salarioHora;  
-    double quotaMensalVendas;  
 
+   private:  
 
-    double pagamentoMes(double horasTrabalhadas) {
- 
-      double t = horasTrabalhadas;
-	  
+	std::string getnome {
+		return nome;
+	}
+	void setnome (std::string n) {
+		n = nome;
+	}
+
+	double getsalarioHora () {
+		return salarioHora;
+	}
+	void setsalrioHora (double s) {
+		setsalarioHora = s;
+	}	
+
+	double getpagamentoMes () {
+		return pagamentoMes;
+	}
+	void setpagamentoMes (double p) {
+		setpagamentoMes = p;
+	}	
+
+       std::string nome;
+       double salarioHora;  
+       const double horasFixas = 8;
+       double pagamentoMes (double horasTrabalhadas){
+	       double horasTotais = horasTrabalhadas;
+       };
+
 	  //Cálculo de hora extra (+50% se horasTrabalhadas > 8)
-      if (horasTrabalhadas > 8) {
-        double x = horasTrabalhadas - 8;
-        t += x / 2;
+         if (horasTrabalhadas > horasFixas) {
+          double horaExtra = horasTrabalhadas - horasFixas;
+          horasTrabalhadas = horasTrabalhas + (horaExtra / 2);
       }
 	  return t * salarioHora;
+		 return horasTrabalhadas * salarioHora;
     }
-	
+
+
+
 };
 
+#endif 
 #endif
